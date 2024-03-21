688a1f9103be05546ce01ba1949f5fb45e9cafbe
phujima <ivaninanovich11122@mail.ru>
2024-03-21 19:48:19 +0300

    Upgrade task 2.1

---
+++ 2.1/contact.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include "contact.h"
+
+void add_contact(Contact *contacts, int *count) {
+    if (*count >= MAX_CONTACTS) {
+        printf("Phonebook is full\n");
+        return;
+    }
+
+    Contact new_contact;
+
+    printf("Enter first name: ");
+    fgets(new_contact.personal_info.first_name, MAX_NAME_LENGTH, stdin);
+    set_first_name(&new_contact.personal_info, new_contact.personal_info.first_name);
+
+    printf("Enter last name: ");
+    fgets(new_contact.personal_info.last_name, MAX_NAME_LENGTH, stdin);
+    set_last_name(&new_contact.personal_info, new_contact.personal_info.last_name);
+
+    printf("Enter work place: ");
+    fgets(new_contact.personal_info.work_place, MAX_NAME_LENGTH, stdin);
+    set_work_place(&new_contact.personal_info, new_contact.personal_info.work_place);
+
+    printf("Enter position: ");
+    fgets(new_contact.personal_info.position, MAX_NAME_LENGTH, stdin);
+    set_position(&new_contact.personal_info, new_contact.personal_info.position);
+
+    printf("Enter phone number: ");
+    fgets(new_contact.contact_info.phone, MAX_PHONE_LENGTH, stdin);
+    set_phone(&new_contact.contact_info, new_contact.contact_info.phone);
+
+    printf("Enter email address: ");
+    fgets(new_contact.contact_info.email, MAX_EMAIL_LENGTH, stdin);
+    set_email(&new_contact.contact_info, new_contact.contact_info.email);
+
+    printf("Enter social media profile: ");
+    fgets(new_contact.contact_info.social, MAX_SOCIAL_LENGTH, stdin);
+    set_social(&new_contact.contact_info, new_contact.contact_info.social);
+
+    contacts[*count] = new_contact;
+    (*count)++;
+}
+
+void edit_contact(Contact *contacts, int count) {
+    if (count == 0) {
+        printf("No contacts to edit\n");
+        return;
+    }
+
+    printf("Enter the index of the contact to edit (0-%d): ", count - 1);
+    int index;
+    scanf("%d", &index);
+    getchar();  // consume newline character
+
+    if (index < 0 || index >= count) {
+        printf("Invalid index\n");
+        return;
+    }
+
+    Contact *contact = &contacts[index];
+
+    char input[MAX_NAME_LENGTH];
+
+    printf("Enter first name: ");
+    fgets(input, MAX_NAME_LENGTH, stdin);
+    set_first_name(&contact->personal_info, input);
+
+    printf("Enter last name: ");
+    fgets(input, MAX_NAME_LENGTH, stdin);
+    set_last_name(&contact->personal_info, input);
+
+    printf("Enter work place: ");
+    fgets(input, MAX_NAME_LENGTH, stdin);
+    set_work_place(&contact->personal_info, input);
+
+    printf("Enter position: ");
+    fgets(input, MAX_NAME_LENGTH, stdin);
+    set_position(&contact->personal_info, input);
+
+    printf("Enter phone number: ");
+    fgets(input, MAX_PHONE_LENGTH, stdin);
+    set_phone(&contact->contact_info, input);
+
+    printf("Enter email address: ");
+    fgets(input, MAX_EMAIL_LENGTH, stdin);
+    set_email(&contact->contact_info, input);
+
+    printf("Enter social media profile: ");
+    fgets(input, MAX_SOCIAL_LENGTH, stdin);
+    set_social(&contact->contact_info, input);
+}
+
+void delete_contact(Contact *contacts, int *count) {
+    if (*count == 0) {
+        printf("No contacts to delete\n");
+        return;
+    }
+
+    printf("Enter the index of the contact to delete (0-%d): ", *count - 1);
+    int index;
+    scanf("%d", &index);
+    getchar();  // consume newline character
+
+    if (index < 0 || index >= *count) {
+        printf("Invalid index\n");
+        return;
+    }
+
+    for (int i = index; i < *count - 1; i++) {
+        contacts[i] = contacts[i + 1];
+    }
+
+    (*count)--;
+}
+
+void print_contact(Contact contact) {
+    printf("First Name: %s\n", get_first_name(&contact.personal_info));
+    printf("Last Name: %s\n", get_last_name(&contact.personal_info));
+    printf("Work Place: %s\n", get_work_place(&contact.personal_info));
+    printf("Position: %s\n", get_position(&contact.personal_info));
+    printf("Phone: %s\n", get_phone(&contact.contact_info));
+    printf("Email: %s\n", get_email(&contact.contact_info));
+    printf("Social: %s\n", get_social(&contact.contact_info));
+    printf("\n");
+}
+
+void print_contacts(Contact *contacts, int count) {
+    if (count == 0) {
+        printf("No contacts\n");
+        return;
+    }
+
+    for (int i = 0; i < count; i++) {
+        printf("Contact %d:\n", i + 1);
+        print_contact(contacts[i]);
+    }
+}
--- 2.1/main.c
+++ 2.1/main.c
@@ -1,179 +1,42 @@
 #include <stdio.h>
-#include <string.h>
-
-#define MAX_CONTACTS 100
-
-struct BasicInfo {
-    char first_name[50];
-    char last_name[50];
-    char sur_name[50];
-    char job_title[50]; 
-};
-
-struct ContactData {
-    char phone[15];
-    char email [50];
-    char social [50];
-    char profile_msg [50];
-};
-
-struct Contact {
-    struct BasicInfo basic_info;
-    struct ContactData contact_data;
-};
-
-void add_contact(struct Contact contacts[], int *num_contacts) {
-    if (*num_contacts >= MAX_CONTACTS) {
-        printf("Contact book is full.\n");
-        return;
-    }
-
-    do {
-        printf("Enter First name: ");
-        scanf("%s", contacts[*num_contacts].basic_info.first_name);
-        printf("Enter Last name: ");
-        scanf("%s", contacts[*num_contacts].basic_info.last_name);
-
-        if (strlen(contacts[*num_contacts].basic_info.first_name) == 0 || 
-            strlen(contacts[*num_contacts].basic_info.last_name) == 0) {
-            printf("Last name and First Name must be filled in!\n");
-        }
-    } while (strlen(contacts[*num_contacts].basic_info.first_name) == 0 || 
-             strlen(contacts[*num_contacts].basic_info.last_name) == 0);
-
-    printf("Enter Surname: ");
-    scanf("%s", contacts[*num_contacts].basic_info.sur_name);
-    printf("Enter job title: ");
-    scanf("%s", contacts[*num_contacts].basic_info.job_title);
-    printf("Enter phone: ");
-    scanf("%s", contacts[*num_contacts].contact_data.phone);
-    printf("Enter email: ");
-    scanf("%s", contacts[*num_contacts].contact_data.email);
-    printf("Enter social media: ");
-    scanf("%s", contacts[*num_contacts].contact_data.social);
-    printf("Enter profile in message: ");
-    scanf("%s", contacts[*num_contacts].contact_data.profile_msg);
-
-    (*num_contacts)++;
-}
-
-void show_contacts(struct Contact contacts[], int num_contacts) {
-    printf("%-15s%-15s%-10s%-15s%-15s%-10s%-10s%-10s\n", "Last Name", "First Name", "Surname",
-     "Job Title", "Phone", "Email", "Social", "Profile Msg");
-
-    for (int i = 0; i < num_contacts; i++) {
-        printf("%-15s%-15s%-10s%-15s%-15s%-10s%-10s%-10s\n",
-            contacts[i].basic_info.last_name,
-            contacts[i].basic_info.first_name,
-            contacts[i].basic_info.sur_name,
-            contacts[i].basic_info.job_title,
-            contacts[i].contact_data.phone,
-            contacts[i].contact_data.email,
-            contacts[i].contact_data.social,
-            contacts[i].contact_data.profile_msg);
-    }
-}
-
-void edit_contact(struct Contact contacts[], int num_contacts) {
-    char edit_first_name[50];
-    char edit_last_name[50];
-
-    printf("Enter the First name of the contact you want to edit: ");
-    scanf("%s", edit_first_name);
-    printf("Enter the Last name of the contact you want to edit: ");
-    scanf("%s", edit_last_name);
-
-    for(int i = 0; i < num_contacts; i++){
-
-        if(strcmp(edit_last_name, contacts[i].basic_info.last_name) == 0 && 
-           strcmp(edit_first_name, contacts[i].basic_info.first_name) == 0){
-            printf("Enter new First name: ");
-            scanf("%s", contacts[i].basic_info.first_name);
-            printf("Enter new Last name: ");
-            scanf("%s", contacts[i].basic_info.last_name);
-            printf("Enter new Surname: ");
-            scanf("%s", contacts[i].basic_info.sur_name);
-            printf("Enter new job title: ");
-            scanf("%s", contacts[i].basic_info.job_title);
-            printf("Enter new phone: ");
-            scanf("%s", contacts[i].contact_data.phone);
-            printf("Enter new email: ");
-            scanf("%s", contacts[i].contact_data.email);
-            printf("Enter new social: ");
-            scanf("%s", contacts[i].contact_data.social);
-            printf("Enter new profile messenger: ");
-            scanf("%s", contacts[i].contact_data.profile_msg);
-
-            printf("Contact edited.\n");
-            return;
-        }
-
-    }
-    printf("Contact not found.\n");
-}
-
-void delete_contact(struct Contact contacts[], int *num_contacts) {
-    char del_first_name[50];
-    char del_last_name[50];
-
-    printf("Enter the First name of the contact you want to delete: ");
-    scanf("%s", del_first_name);
-    printf("Enter the Last name of the contact you want to delete: ");
-    scanf("%s", del_last_name);
-
-    for (int i = 0; i < *num_contacts; i++) {
-
-        if (strcmp(del_first_name, contacts[i].basic_info.first_name) == 0 &&
-            strcmp(del_last_name, contacts[i].basic_info.last_name) == 0) {
-                
-                for(int j = i; j < *num_contacts - 1; j++){
-                    contacts[j] = contacts[j + 1];
-                }
-
-                (*num_contacts)--;
-                printf("Contact deleted.\n");
-                return;
-            }
-    }
-    printf("Contact not found.\n");
-}
+#include "contact.h"
 
 int main() {
-   int choice;
-   struct Contact contacts[MAX_CONTACTS];
-   int num_contacts = 0;
+    Contact contacts[MAX_CONTACTS];
+    int count = 0;
+    char choice;
 
-   do{
-        printf("\nPhone Book Menu:\n");
-        printf("1. Add Contact\n");
-        printf("2. Show Contacts\n");
-        printf("3. Edit Contact\n");
-        printf("4. Delete Contact\n");
+    do {
+        printf("Menu:\n");
+        printf("1. Add contact\n");
+        printf("2. Edit contact\n");
+        printf("3. Delete contact\n");
+        printf("4. Print contacts\n");
         printf("5. Exit\n");
         printf("Enter your choice: ");
-        scanf("%d", &choice);
+        scanf(" %c", &choice);
+        getchar(); // Consume newline character
 
-        switch (choice)
-        {
-        case 1:
-            add_contact(contacts, &num_contacts);
-            break;
-        case 2:
-            show_contacts(contacts, num_contacts);
-            break;
-        case 3:
-            edit_contact(contacts, num_contacts);
-            break;
-        case 4:
-            delete_contact(contacts, &num_contacts);
-            break;
-        case 5:
-            printf("Exiting...\n");
-            break;
-        
-        default:
-            printf("Invalid choice.\n");
+        switch(choice) {
+            case '1':
+                add_contact(contacts, &count);
+                break;
+            case '2':
+                edit_contact(contacts, count);
+                break;
+            case '3':
+                delete_contact(contacts, &count);
+                break;
+            case '4':
+                print_contacts(contacts, count);
+                break;
+            case '5':
+                printf("Exiting program\n");
+                break;
+            default:
+                printf("Invalid choice\n");
         }
-   } while(choice != 5);
+    } while(choice != '5');
+
     return 0;
 }
