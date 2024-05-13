835e1b2188eb694fd9914408254c0cfc35479e01
phygima <dontgivefucc@ya.ru>
2024-05-13 06:30:42 +0000

    Task 6.2

---
+++ Module_3/6.*/6.2/client.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 12345
+#define MAX_MSG_SIZE 1024
+
+int main() {
+    int sockfd;
+    char buffer[MAX_MSG_SIZE];
+    struct sockaddr_in servaddr;
+ 
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+ 
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    
+    int n, len;
+    
+    while (1) {
+        printf("Me: ");
+        fgets(buffer, MAX_MSG_SIZE, stdin);
+ 
+        sendto(sockfd, (const char *)buffer, strlen(buffer), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+ 
+        n = recvfrom(sockfd, (char *)buffer, MAX_MSG_SIZE, MSG_WAITALL, (struct sockaddr *)&servaddr, &len);
+        buffer[n] = '\0';
+ 
+        printf("Other: %s\n", buffer);
+    }
+    
+    close(sockfd);
+    
+    return 0;
+}
